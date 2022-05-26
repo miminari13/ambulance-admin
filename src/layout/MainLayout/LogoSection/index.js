53fc1544f157a2fe111d83e30c01efc1df3dddf7
Mir <miminari13@gmail.com>
2022-05-26 13:57:12 +0300

    Removed original meta attrs

--- src/layout/MainLayout/LogoSection/index.js
+++ src/layout/MainLayout/LogoSection/index.js
@@ -5,13 +5,21 @@ import { ButtonBase } from '@mui/material';
 
 // project imports
 import config from 'config';
-import Logo from 'ui-component/Logo';
+import Logo from 'assets/images/logo.png';
+// import Logo from 'ui-component/Logo';
 
 // ==============================|| MAIN LOGO ||============================== //
+const titleStyle = {
+    marginLeft: '.5rem',
+    lineHeight: 1,
+    fontWeight: 600
+};
 
 const LogoSection = () => (
     <ButtonBase disableRipple component={Link} to={config.defaultPath}>
-        <Logo />
+        <img src={Logo} alt="MIR Amb" width="30" height="30" />
+        <h3 style={titleStyle}>MIR.Amb</h3>
+        {/* <Logo /> */}
     </ButtonBase>
 );
 
